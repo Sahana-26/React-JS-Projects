9903d687dbf4fbfa07a1097f9e25bad80727e2eb
Sahana <sahanaa2602@gmail.com>
2024-10-24 15:23:35 +0530

    initial file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Basic from './component/Basic'
+import Cards from './component/Cards/Cards'
+
+function App(){
+  return(
+    <>
+    <Basic/>
+    <ImagGalery/>
+    <Cards/>
+    
+    </>
+    
+  )}
+export default App
